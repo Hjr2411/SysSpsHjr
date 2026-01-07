59d6f29452e5e333e03e91d86dfeec6683872f04
Helio Junior <87399421+Hjr2411@users.noreply.github.com>
2026-01-07 18:24:59 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,7 @@
 // firebase-config.js
+// SysSPS – Firebase
+// Versão: v2.0.0
+
 const firebaseConfig = {
   apiKey: "AIzaSyAeCrURSs0TBXlYF3TKLi4q98VwrGaKe_Q",
   authDomain: "spsch-849e5.firebaseapp.com",
@@ -6,11 +9,11 @@ const firebaseConfig = {
   projectId: "spsch-849e5",
   storageBucket: "spsch-849e5.firebasestorage.app",
   messagingSenderId: "698967090558",
-  appId: "1:698967090558:web:978781fd27b86c36203f2f",
-  measurementId: "G-C5D3774P2G"
+  appId: "1:698967090558:web:978781fd27b86c36203f2f"
 };
 
-// firebase-config.js
-const app = firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
+
+console.log("🔥 Firebase inicializado (v2.0.0)");
 
